afe1266a32acd2f94ac38b1c52a5cba94866e43b
Kristopher Parrillo <kkparrillo@gmail.com>
2017-01-17 12:34:22 -0800

    server working

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
 
-var app = express();
+var routes = require('./routes/index');
 
+var app = express();
 
 
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/'}));
 
-app.set('', path.join(__dirname, ''))
+app.set('views', path.join(__dirname, 'views'))
 
 app.set('view engine', 'hbs');
 
@@ -19,12 +20,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', routes)
 
 
 
-
-
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port,function(){
   console.log('listening on port ' + port);
 })
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 var express = require('express');
+var router = express.Router();
 var mongo = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 var assert = require('assert');
 
 var url = 'mongodb://localhost:27017/BURST';
 
+router.get('/', function(req, res, next) {
+  res.render('card', {title: 'Burst Card Gallery'})
+})
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
