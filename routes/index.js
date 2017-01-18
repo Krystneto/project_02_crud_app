a16170acb637647d8429d2e1632c8c051c008115
Kristopher Parrillo <kkparrillo@gmail.com>
2017-01-17 18:33:51 -0800

    more data

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,38 @@ var mongo = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 var assert = require('assert');
 
-var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/BURST';
+var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/Burst';
 
+// Home Page
 router.get('/', function(req, res, next) {
-  res.render('card', {title: 'Burst Card Gallery'})
+  mongo.connect(url, function(err, db) {
+    db.collection('card_set').find({}).toArray(function(err, results) {
+    assert.equal(null, err);
+    console.log('Pulling cards from card_set collection');
+    db.close();
+    });
+  });
+  res.render('card', {
+    title: 'Burst Card Gallery',
+    name: results.name,
+    spot_number: results.spot_number,
+    spot_type: results.spot_type,
+    aura: results.aura,
+    type: results.type,
+    class: results.class,
+    ability: results.ability,
+    burst: results.burst,
+    attack: results.attack,
+    defense: results.defense,
+  })
 })
 
+// Create a card page
 router.get('/create-a-card', function(req, res, next) {
-
+  res.render('create');
 })
 
-router.get('/update', function(req, res, next) {
-
-})
-
-router.get('/delete', function(req, res, next) {
-
-})
-
-router.get('/update/:name', function(req, res, next) {
-
-})
-
+// Insert card information into database
 router.post('/insert_card', function(req, res, next) {
   var card = {
     name: req.body.name,
@@ -38,8 +48,36 @@ router.post('/insert_card', function(req, res, next) {
     burst: req.body.burst,
     attack: req.body.attack,
     defense: req.body.defense,
-  }
+  };
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('card_set').insertOne(card, function(err, result){
+      assert.equal(null, err);
+      console.log(card.name + 'inserted into the card_set');
+      db.close();
+    })
+  })
+});
+
+
+// Update a card page
+router.get('/update', function(req, res, next) {
+  res.render('update')
 })
+
+// Delete a card page
+router.get('/delete', function(req, res, next) {
+  res.render('delete')
+})
+
+router.get('/')
+
+
+router.post('/update/:name', function(req, res, next) {
+
+})
+
+
 
 
 
