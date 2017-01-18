f60b5b9dc8b77ebf78b559acb7fe6be3832baf37
Kristopher Parrillo <kkparrillo@gmail.com>
2017-01-17 16:31:58 -0800

    adding routes

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,36 @@ router.get('/', function(req, res, next) {
   res.render('card', {title: 'Burst Card Gallery'})
 })
 
+router.get('/create-a-card', function(req, res, next) {
 
+})
 
+router.get('/update', function(req, res, next) {
 
+})
 
+router.get('/delete', function(req, res, next) {
 
+})
 
+router.get('/update/:name', function(req, res, next) {
 
+})
+
+router.post('/insert_card', function(req, res, next) {
+  var card = {
+    name: req.body.name,
+    spot_number: req.body.spot_number,
+    spot_type: req.body.spot_type,
+    aura: req.body.aura,
+    type: req.body.type,
+    class: req.body.class,
+    ability: req.body.ability,
+    burst: req.body.burst,
+    attack: req.body.attack,
+    defense: req.body.defense,
+  }
+})
 
 
 
