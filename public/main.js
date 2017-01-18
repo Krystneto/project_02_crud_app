aff42883c833f3b999ff5efde4776cbedfec032c
Kristopher Parrillo <kkparrillo@gmail.com>
2017-01-18 15:52:15 -0800

    create card to db

--- public/main.js
+++ public/main.js
@@ -1,8 +1,32 @@
 console.log('Hello from public');
 
-$('.edit').on('click', function(evt){
+
+
+
+
+// edit button handler
+$('.edit').on('click', function(evt) {
   console.log('clicked');
-  $.get('/card/judge', function(response){
+  $.get('/card/judge', function(response) {
     console.log(response);
+  })
+})
+
+$('.create').on('click', function(evt) {
+  console.log('clicked');
+  $.post('/insert_card', {
+    name:$('input[name="name"]').val(),
+    spot_number:$("input[name='spot_number']").val(),
+    spot_type:$("input[name='spot_type']").val(),
+    aura:$("input[name='aura']").val(),
+    type:$("input[name='type']").val(),
+    group: $("input[name='group']").val(),
+    ability: $("textarea[name='ability']").val(),
+    burst:$("input[name='burst']").val(),
+    attack:$("input[name='attack']").val(),
+    defense:$("input[name='defense']").val(),
+  },
+  function(response) {
+    console.log(response)
   })
 })
